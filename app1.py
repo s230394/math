697a19d38e3da7fc81ed58033cd00b8722be2884
s230394 <gimpo.s230394@ggh.goe.go.kr>
2024-10-31 11:36:14 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,11 +2,11 @@
 import streamlit as st      
 
 # 제목 쓰기
-st.title('동생아 _________')  
+st.title('동생아 수학 공부하자')  
 # 부제목 쓰기
-st.subheader('오늘의 주제: _______')
+st.subheader('오늘의 주제: 양수와 음수')
 # 본문 쓰기
-st.write('음수?? ______않아!') 
+st.write('음수?? 어렵지 않아!') 
 
 # 여러 개의 열(문단)을 생성
 #col1, col2 = st.columns(2)       
@@ -19,7 +19,7 @@ st.write('음수?? ______않아!')
 #      st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
 # 오른쪽 문단
 #with col2:
-#      st.image('______________')  # 이미지 파일 불러오기
+#      st.image('')  # 이미지 파일 불러오기
 
 # 사용자의 입력을 받아서 a에 저장하기(초기값은 0)
 #a = st.number_input('____________', value= ____)  
